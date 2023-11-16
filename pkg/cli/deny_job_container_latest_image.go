36b4f9dc414776e82d9f544fa6ee2d32e021b684
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-11-16 19:32:42 +0900

    feat: add a policy `deny_job_container_latest_image`

---
+++ pkg/cli/deny_job_container_latest_image.go
@@ -0,0 +1,44 @@
+package cli
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+type DenyJobContainerLatestImagePolicy struct{}
+
+func (p *DenyJobContainerLatestImagePolicy) Name() string {
+	return "deny_job_container_latest_image"
+}
+
+func (p *DenyJobContainerLatestImagePolicy) Apply(ctx context.Context, logE *logrus.Entry, cfg *Config, wf *Workflow) error {
+	failed := false
+	for jobName, job := range wf.Jobs {
+		logE := logE.WithField("job_name", jobName)
+		if job.Container == nil {
+			continue
+		}
+		if job.Container.Image == "" {
+			logE.Error("job container should have image")
+			failed = true
+			continue
+		}
+		_, tag, ok := strings.Cut(job.Container.Image, ":")
+		if !ok {
+			logE.Error("job container image should be <image name>:<tag>")
+			failed = true
+			continue
+		}
+		if tag == "latest" {
+			logE.Error("job container image tag should not be `latest`")
+			failed = true
+		}
+	}
+	if failed {
+		return errors.New("workflow violates policies")
+	}
+	return nil
+}
--- pkg/cli/run.go
+++ pkg/cli/run.go
@@ -41,6 +41,7 @@ func (r *Runner) Run(ctx *cli.Context) error {
 		&DenyReadAllPermissionPolicy{},
 		&DenyWriteAllPermissionPolicy{},
 		&DenyInheritSecretsPolicy{},
+		&DenyJobContainerLatestImagePolicy{},
 	}
 	failed := false
 	for _, filePath := range filePaths {
@@ -78,106 +79,4 @@ func (r *Runner) validateWorkflow(ctx *cli.Context, logE *logrus.Entry, cfg *Con
 type Policy interface {
 	Name() string
 	Apply(ctx context.Context, logE *logrus.Entry, cfg *Config, wf *Workflow) error
-}
-
-type Workflow struct {
-	FilePath    string `yaml:"-"`
-	Jobs        map[string]*Job
-	Env         map[string]string
-	Permissions *Permissions
-}
-
-type Job struct {
-	Permissions *Permissions
-	Env         map[string]string
-	Steps       []interface{}
-	Secrets     *JobSecrets
-}
-
-type Permissions struct {
-	m        map[string]string
-	readAll  bool
-	writeAll bool
-}
-
-func (permissions *Permissions) Permissions() map[string]string {
-	if permissions == nil {
-		return nil
-	}
-	return permissions.m
-}
-
-func (permissions *Permissions) ReadAll() bool {
-	if permissions == nil {
-		return false
-	}
-	return permissions.readAll
-}
-
-func (permissions *Permissions) WriteAll() bool {
-	if permissions == nil {
-		return false
-	}
-	return permissions.writeAll
-}
-
-func (permissions *Permissions) IsNil() bool {
-	if permissions == nil {
-		return true
-	}
-	return permissions.m == nil && !permissions.readAll && !permissions.writeAll
-}
-
-func (permissions *Permissions) UnmarshalYAML(unmarshal func(interface{}) error) error {
-	var val interface{}
-	if err := unmarshal(&val); err != nil {
-		return err
-	}
-	return convPermissions(val, permissions)
-}
-
-func convPermissions(src interface{}, dest *Permissions) error { //nolint:cyclop
-	switch p := src.(type) {
-	case string:
-		switch p {
-		case "read-all":
-			dest.readAll = true
-			return nil
-		case "write-all":
-			dest.writeAll = true
-			return nil
-		default:
-			return logerr.WithFields(errors.New("unknown permissions"), logrus.Fields{ //nolint:wrapcheck
-				"permission": p,
-			})
-		}
-	case map[interface{}]interface{}:
-		m := make(map[string]string, len(p))
-		for k, v := range p {
-			ks, ok := k.(string)
-			if !ok {
-				return errors.New("permissions key must be string")
-			}
-			vs, ok := v.(string)
-			if !ok {
-				return errors.New("permissions value must be string")
-			}
-			m[ks] = vs
-		}
-		dest.m = m
-		return nil
-	case map[string]interface{}:
-		m := make(map[string]string, len(p))
-		for k, v := range p {
-			vs, ok := v.(string)
-			if !ok {
-				return errors.New("permissions value must be string")
-			}
-			m[k] = vs
-		}
-		dest.m = m
-		return nil
-	default:
-		return errors.New("permissions must be map[string]string or 'read-all' or 'write-all'")
-	}
 }
