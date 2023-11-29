c0cec6def723b9f2e4986821cc137e3137ea2ca4
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2023-11-29 15:34:37 +0900

    fix: output errors regarding to configuration files (#241)

---
+++ pkg/controller/error.go
@@ -0,0 +1,23 @@
+package controller
+
+import "github.com/sirupsen/logrus"
+
+type HasLogLevelError struct {
+	LogLevel logrus.Level
+	Err      error
+}
+
+func (e *HasLogLevelError) Error() string {
+	return e.Err.Error()
+}
+
+func (e *HasLogLevelError) Unwrap() error {
+	return e.Err
+}
+
+func debugError(err error) *HasLogLevelError {
+	return &HasLogLevelError{
+		LogLevel: logrus.DebugLevel,
+		Err:      err,
+	}
+}
