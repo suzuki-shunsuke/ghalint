83b7a2e6e9cb6581da87e7e517422c2d99b37dea
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2023-12-02 16:57:58 +0900

    feat: add a link to the document (#254)
    * feat: add a link to the document
    
    * feat: add a link to the document

--- pkg/workflow/read_workflow.go
+++ pkg/workflow/read_workflow.go
@@ -1,9 +1,13 @@
 package workflow
 
 import (
+	"errors"
 	"fmt"
+	"io"
 
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/afero"
+	"github.com/suzuki-shunsuke/logrus-error/logerr"
 	"gopkg.in/yaml.v3"
 )
 
@@ -14,7 +18,13 @@ func Read(fs afero.Fs, p string, wf *Workflow) error {
 	}
 	defer f.Close()
 	if err := yaml.NewDecoder(f).Decode(wf); err != nil {
-		return fmt.Errorf("parse a workflow file as YAML: %w", err)
+		err := fmt.Errorf("parse a workflow file as YAML: %w", err)
+		if errors.Is(err, io.EOF) {
+			return logerr.WithFields(err, logrus.Fields{ //nolint:wrapcheck
+				"reference": "https://github.com/suzuki-shunsuke/ghalint/blob/main/docs/codes/001.md",
+			})
+		}
+		return err
 	}
 	return nil
 }
