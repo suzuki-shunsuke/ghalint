ee9832b7c0616828a072e77344ab9e4279adb472
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-12-08 23:10:45 +0900

    style: format

--- pkg/policy/error.go
+++ pkg/policy/error.go
@@ -3,8 +3,8 @@ package policy
 import "errors"
 
 var (
-	errPermissionsIsRequired   = errors.New("the input `permissions` is required")
-	errRepositoriesIsRequired  = errors.New("the input `repositories` is required")
-	errJobViolatePolicy      = errors.New("the job violates policies")
-	errWorkflowViolatePolicy = errors.New("the workflow violates policies")
+	errPermissionsIsRequired  = errors.New("the input `permissions` is required")
+	errRepositoriesIsRequired = errors.New("the input `repositories` is required")
+	errJobViolatePolicy       = errors.New("the job violates policies")
+	errWorkflowViolatePolicy  = errors.New("the workflow violates policies")
 )
