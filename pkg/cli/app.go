3719e582a10c6b397024604efa2dc23658b8b708
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2025-04-06 08:39:20 +0900

    fix: fix version (#840)

--- pkg/cli/app.go
+++ pkg/cli/app.go
@@ -1,11 +1,10 @@
 package cli
 
 import (
-	"context"
-
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/afero"
-	"github.com/suzuki-shunsuke/urfave-cli-v3-help-all/helpall"
+	"github.com/suzuki-shunsuke/urfave-cli-v3-util/helpall"
+	"github.com/suzuki-shunsuke/urfave-cli-v3-util/vcmd"
 	"github.com/urfave/cli/v3"
 )
 
@@ -26,63 +25,60 @@ type Runner struct {
 }
 
 func New(flags *LDFlags, fs afero.Fs, logE *logrus.Entry) *cli.Command {
-	app := &cli.Command{}
-	app.Name = "ghalint"
-	app.Usage = "GitHub Actions linter"
-	app.Version = flags.AppVersion()
-	app.Flags = []cli.Flag{
-		&cli.StringFlag{
-			Name:  "log-color",
-			Usage: "log color. auto(default)|always|never",
-			Sources: cli.EnvVars(
-				"GHALINT_LOG_COLOR",
-			),
-		},
-		&cli.StringFlag{
-			Name:  "log-level",
-			Usage: "log level",
-			Sources: cli.EnvVars(
-				"GHALINT_LOG_LEVEL",
-			),
-		},
-		&cli.StringFlag{
-			Name:    "config",
-			Aliases: []string{"c"},
-			Usage:   "configuration file path",
-			Sources: cli.EnvVars(
-				"GHALINT_CONFIG",
-			),
-		},
-	}
 	runner := &Runner{
 		flags: flags,
 		fs:    fs,
 		logE:  logE,
 	}
-	app.Commands = []*cli.Command{
-		{
-			Name:   "run",
-			Usage:  "lint GitHub Actions Workflows",
-			Action: runner.Run,
-			Flags:  []cli.Flag{},
-		},
-		{
-			Name: "run-action",
-			Aliases: []string{
-				"act",
+	return helpall.With(&cli.Command{
+		Name:    "ghalint",
+		Usage:   "GitHub Actions linter",
+		Version: flags.AppVersion(),
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "log-color",
+				Usage: "log color. auto(default)|always|never",
+				Sources: cli.EnvVars(
+					"GHALINT_LOG_COLOR",
+				),
 			},
-			Usage:  "lint actions",
-			Action: runner.RunAction,
-			Flags:  []cli.Flag{},
-		},
-		{
-			Name:  "version",
-			Usage: "Show version",
-			Action: func(_ context.Context, cmd *cli.Command) error {
-				cli.ShowVersion(cmd)
-				return nil
+			&cli.StringFlag{
+				Name:  "log-level",
+				Usage: "log level",
+				Sources: cli.EnvVars(
+					"GHALINT_LOG_LEVEL",
+				),
+			},
+			&cli.StringFlag{
+				Name:    "config",
+				Aliases: []string{"c"},
+				Usage:   "configuration file path",
+				Sources: cli.EnvVars(
+					"GHALINT_CONFIG",
+				),
 			},
 		},
-	}
-	return helpall.With(app, nil)
+		Commands: []*cli.Command{
+			{
+				Name:   "run",
+				Usage:  "lint GitHub Actions Workflows",
+				Action: runner.Run,
+				Flags:  []cli.Flag{},
+			},
+			{
+				Name: "run-action",
+				Aliases: []string{
+					"act",
+				},
+				Usage:  "lint actions",
+				Action: runner.RunAction,
+				Flags:  []cli.Flag{},
+			},
+			vcmd.New(&vcmd.Command{
+				Name:    "ghalint",
+				Version: flags.AppVersion(),
+				SHA:     flags.Commit,
+			}),
+		},
+	}, nil)
 }
