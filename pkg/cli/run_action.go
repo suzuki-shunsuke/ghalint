58056838576c468d3fdf672d03ea0b00d6a9fc34
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-06-15 08:36:14 +0900

    refactor: replace log package

--- pkg/cli/run_action.go
+++ pkg/cli/run_action.go
@@ -2,21 +2,18 @@ package cli
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/suzuki-shunsuke/ghalint/pkg/controller/act"
-	"github.com/suzuki-shunsuke/ghalint/pkg/log"
+	"github.com/suzuki-shunsuke/urfave-cli-v3-util/log"
 	"github.com/urfave/cli/v3"
 )
 
 func (r *Runner) RunAction(ctx context.Context, cmd *cli.Command) error {
 	logE := r.logE
 
-	if color := cmd.String("log-color"); color != "" {
-		log.SetColor(color, logE)
-	}
-
-	if logLevel := cmd.String("log-level"); logLevel != "" {
-		log.SetLevel(logLevel, logE)
+	if err := log.Set(logE, cmd.String("log-level"), cmd.String("log-color")); err != nil {
+		return fmt.Errorf("configure logger: %w", err)
 	}
 
 	ctrl := act.New(r.fs)
